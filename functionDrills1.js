ea81835599732661de3e7d7b094cb80e65419d87
Darian Patterson <Darian@Darians-MacBook-Pro.local>
2020-01-06 15:24:27 -0500

    Drill 2 and 3

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,1 +1,6 @@
-console.log("Hi, my name is Daniel and I'm 27 years old");
+function createGreeting () {
+    return ("Hi, my name is Daniel and I'm 27 years old");
+}
+let greeting1 = createGreeting();
+console.log(greeting1);
+createGreeting();
