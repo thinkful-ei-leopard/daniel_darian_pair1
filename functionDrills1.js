64713aaf767989e33a34d1629ffd9866b6ed201c
Darian Patterson <Darian@Darians-MacBook-Pro.local>
2020-01-06 16:12:06 -0500

    exercise 6 and 7

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,12 +1,22 @@
 function createGreeting (name, age) {
     const yearOfBirth = (2020-age);
-    name = 'Daniel';
-    age = 27;
     return `Hi, my name is ${name} and I am ${age}. I was born in ${yearOfBirth}`;
 }
-let greeting1 = createGreeting();
+let greeting1 = createGreeting("Daniel", 27);
 console.log(greeting1);
 
 let subtraction =5;
 const year = 2020-subtraction;
 console.log(year);
+
+function getYearOfBirth (age) {
+if (age < 0){
+    throw new Error('Age can not be negative');
+}
+    return (2020-age);
+}
+try {
+    const greeting1 = createGreeting ("Zeus", 3456);
+} catch (e){
+    console.log (e.name);
+}
