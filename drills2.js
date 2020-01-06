31e4c19005e445b396e13f93602ae39a3c3105ce
Darian Patterson <Darian@Darians-MacBook-Pro.local>
2020-01-06 17:01:34 -0500

    to infinity

--- drills2.js
+++ drills2.js
@@ -4,3 +4,19 @@ function jediName(firstName, lastName) {
     return jedi1.concat(jedi2);
 }
 console.log(jediName("Daniel", "Fong"));
+
+function beyond (num){
+    if (num === Infinity){
+    return 'And beyond';
+    }
+    else if (num > 0){
+       return 'To Infinity'; 
+    }
+    else if (num < 0){
+        return 'To negative infinity';
+    }
+    else if (num === 0){
+        return 'Staying home';
+    }
+}
+console.log (beyond (-6));
